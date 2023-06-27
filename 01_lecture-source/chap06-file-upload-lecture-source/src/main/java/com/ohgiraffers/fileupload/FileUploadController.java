527b33b420812d6ab06a46cbdb998c7b26a166b6
jinvvoo <jinvvookim@gmail.com>
2023-06-27 10:17:48 +0900

    file upload 메인창에 파일업로드, 파일 선택기능, 제출시 콘솔출력가능 업로드 파일 생성됨, 업로드 한 파일명 변경해주는 작업, 파일 업로드 성공! 실패한 파일 삭제!
    멀티 파일 업로드까지 성공!

--- 01_lecture-source/chap06-file-upload-lecture-source/src/main/java/com/ohgiraffers/fileupload/FileUploadController.java
+++ 01_lecture-source/chap06-file-upload-lecture-source/src/main/java/com/ohgiraffers/fileupload/FileUploadController.java
@@ -9,7 +9,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.UUID;
+import java.util.*;
 
 @Controller
 public class FileUploadController {
@@ -17,7 +17,7 @@ public class FileUploadController {
     @PostMapping("single-file")
     public String singleFileUpload(@RequestParam MultipartFile singleFile,
                                    @RequestParam String singleFileDescription,
-                                   Model model){
+                                   Model model) {
 
         System.out.println("singleFile = " + singleFile);
         System.out.println("singleFileDescription = " + singleFileDescription);
@@ -47,6 +47,54 @@ public class FileUploadController {
 //            throw new RuntimeException(e);
         }
 
+
+        return "result";
+    }
+
+    @PostMapping("multi-file")
+    public String multiFileUpload(@RequestParam List<MultipartFile> multipartFiles,
+                                  @RequestParam String multiFileDescription,
+                                  Model model) {
+        System.out.println("multipartFiles = " + multipartFiles);
+        System.out.println("multiFileDescription = " + multiFileDescription);
+
+
+        String root = "C:\\app-file";
+        String filePath = root + "/uploadFiles";
+
+        File dir = new File(filePath);
+        if (!dir.exists()) {
+            dir.mkdirs(); //루드경로 딱하나 폴더만들면 mkdir, 여러개의 폴더면 mkdirs
+        }
+
+        List<Map<String, String >> files = new ArrayList<>();
+        for (int i = 0; i < multipartFiles.size(); i++) {
+            String originFileName = multipartFiles.get(i).getOriginalFilename();
+            String ext = originFileName.substring(originFileName.lastIndexOf("."));
+            String savedNae = UUID.randomUUID().toString().replaceAll("-", "") + ext;
+
+            Map<String, String> file = new HashMap<>();
+            file.put("originFileName", originFileName);
+            file.put("savedName", savedNae);
+            file.put("filePath", filePath);
+
+            files.add(file);
+        }
+
+        try {
+            for (int i = 0; i < multipartFiles.size(); i++) {
+                Map<String, String> file = files.get(i);
+                multipartFiles.get(i).transferTo(new File(filePath + "/" + file.get("savedName")));
+            }
+
+            model.addAttribute("message", "파일 업로드 성공!");
+        } catch (Exception e) {
+            for (int i = 0; i < multipartFiles.size(); i++) {
+                Map<String, String> file = files.get(i);
+                new File(filePath + "/" + file.get("savedName")).delete();
+            }
+            model.addAttribute("message", "파일 업로드 실패!");
+        }
 
         return "result";
     }
