f4e55e823c3276bbec98ffc3657bdf99caf5cc4f
jinvvoo <jinvvookim@gmail.com>
2023-06-27 09:10:55 +0900

    file upload 메인페이지 보이는지

---
+++ 01_lecture-source/chap06-file-upload-lecture-source/src/main/java/com/ohgiraffers/fileupload/MainController.java
@@ -0,0 +1,21 @@
+package com.ohgiraffers.fileupload;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/*")
+public class MainController {
+
+    @GetMapping("/")
+    public String defualutLocation() {
+
+        return "main";
+    }
+
+    @GetMapping("/main")
+    public void main() {}
+
+
+}
